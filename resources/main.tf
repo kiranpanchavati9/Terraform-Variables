8146a8781664642887cf75065673b4ae99a5b768
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-03 13:23:14 +0400

    created new splunk instances and firewall rules

--- resources/main.tf
+++ resources/main.tf
@@ -1,12 +1,4 @@
-resource "aws_instance" "splunk-components" {
-  count = 3
-  ami           = var.ami
-  instance_type = var.type
-
-  tags = {
-    Name = var.components[count.index]
-  }
-}
+# Create Security Group
 
 resource "aws_security_group" "allow_splunk_firewall_rules" {
   name        = "allow_splunk_firewall"
@@ -28,6 +20,7 @@ resource "aws_vpc_security_group_ingress_rule" "splunk" {
   to_port           = tonumber(each.value)
 }
 
+# Create Ingress Rules
 
 resource "aws_vpc_security_group_egress_rule" "splunk" {
   for_each = toset(var.ports)
@@ -38,3 +31,17 @@ resource "aws_vpc_security_group_egress_rule" "splunk" {
   ip_protocol       = var.protocol
   to_port           = tonumber(each.value)
 }
+
+# Create Instance and Attach SG at launch
+
+resource "aws_instance" "splunk-components" {
+  count = 3
+  ami           = var.ami
+  instance_type = var.type
+  vpc_security_group_ids = [aws_security_group.allow_splunk_firewall_rules.id]
+
+tags = {
+    Name = var.components[count.index]
+  }
+}
+
