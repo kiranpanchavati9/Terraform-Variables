d85dfcbf89d4a81c88d61b1b749e0665e71d0e82
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-03 13:06:23 +0400

    created new splunk instances and firewall rules

--- resources/main.tf
+++ resources/main.tf
@@ -14,11 +14,22 @@ resource "aws_security_group" "allow_splunk_firewall_rules" {
   vpc_id      = var.vpcid
 
   tags = {
-    Name = "allow_splunk_firewall"
+    Name = "allow_splunk_firewall_ports"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "splunk" {
+  for_each = toset(var.ports)
+
+  security_group_id = aws_security_group.allow_splunk_firewall_rules.id
+  cidr_ipv4         = var.cidripv4
+  from_port         = tonumber(each.value)
+  ip_protocol       = var.protocol
+  to_port           = tonumber(each.value)
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "splunk" {
   for_each = toset(var.ports)
 
   security_group_id = aws_security_group.allow_splunk_firewall_rules.id
